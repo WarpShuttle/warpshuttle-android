3bce446db289478a8aa52ed96950927df532f306
Ronit Prajapati <ronitprajapati91@gmail.com>
2024-09-17 07:47:01 +0545

    working on splash screen on jetpack compose

--- app/src/main/java/com/android/warpshuttle/app/MainActivity.kt
+++ app/src/main/java/com/android/warpshuttle/app/MainActivity.kt
@@ -3,27 +3,40 @@ package com.android.warpshuttle.app
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.android.warpshuttle.R
+import com.android.warpshuttle.presentation.feature.splashscreen.SplashScreen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var keepSplashOnScreen = true
-        val delay = 2000L
+//        var keepSplashOnScreen = true
+//        val delay = 2000L
+//
+//        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
+//        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
 
-        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
-        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        installSplashScreen()
+        setContent {
+            WarpShuttleSplashScreenTheme{
+
+            }
         }
     }
 }
